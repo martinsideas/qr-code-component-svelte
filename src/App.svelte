3ec6128a81d058f196686ed43bf03bf25345c62d
Martin The Coder <acadecoder@gmail.com>
2022-02-17 18:13:59 -0500

    Mobile version (375px) finished.

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import QrCode from './components/QrCode.svelte'
+
+	let styles = {
+		App: `
+		bg-blue-100
+		h-screen
+		flex justify-center items-center
+		`
+	}
+</script>
+
+<div class={styles.App}>
+	<QrCode/>
+</div>
---
+++ src/components/QrCode.js
@@ -0,0 +1,2 @@
+export let title = 'Improve your front-end skills by building projects'
+export let description = 'Scan the QR code to visit Frontend Mentor and take your coding skills to the next level'
